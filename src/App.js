c2f195fbc51156b571ac612e3bae6c8479a457d9
kevin <tenngawang6080@gmail.com>
2020-06-17 20:40:10 -0400

    first commnit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    assets: []
+  }
+
+  componentDidMount = () => {
+    fetch('https://api.coincap.io/v2/assets')
+  .then(response => response.json())
+  .then(data => this.setState({assets: data.data}));
+  }
+
+  renderAssets = () => {
+    console.log("state: ", this.state.assets[1])
+    if(this.state.assets.length > 0){
+      return this.state.assets.map(asset => <h4>{asset.synbol}</h4>)
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+       {this.renderAssets()}
+      </div>
+    );
+  }
 }
 
 export default App;
