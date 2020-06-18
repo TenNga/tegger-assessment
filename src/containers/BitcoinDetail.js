051869c1d594e24170160a415267e877bea5ba96
kevin <tenngawang6080@gmail.com>
2020-06-17 22:30:16 -0400

    added button for four column and three

---
+++ src/containers/BitcoinDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BitcoinDetail = () => {
+    return(
+        <h1>Bitcoin Detail Page</h1>
+    )
+}
+
+export default BitcoinDetail;
--- src/containers/CryptoContainer.js
+++ src/containers/CryptoContainer.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import CryptoCard from '../components/CryptoCard';
+import './css/CryptoContainer.css';
 
-const CryptoContainer = ({assets}) => {
-    const renderCards = () => {
-        if(assets.length > 1){
-           return assets.map(asset => <CryptoCard data={asset} key={asset.id}/>)
+class CryptoContainer extends React.Component {
+    state = {
+        column: 3
+    }
+
+    renderCards = () => {
+        if(this.props.assets.length > 1){
+           return this.props.assets.map(asset => <CryptoCard data={asset} key={asset.id}/>)
         }
     }
-    return(
-        <>
-         {renderCards()}
-        </>
-    )
+
+    handleFourColumn = () => this.setState({column: 4})
+
+    handleThreeColumn = () => this.setState({column: 3})
+
+
+    handleColumn = () => {
+        if(this.state.column == 3){
+            return (
+                <div id="card-container-three">
+                    {this.renderCards()}
+                </div>
+            )
+        }
+        else {
+            return(
+                <div id="card-container-four">
+                    {this.renderCards()}
+                </div>
+            )
+        }
+    }
+
+    render(){
+        return(
+            <>
+                <h1>Welcome To Cryotocurrency Information</h1>
+                <div className="header">
+                    <button onClick={this.handleFourColumn}>Four Column</button>
+
+                    <button onClick={this.handleThreeColumn}>Three Column</button>
+                </div>
+                {this.handleColumn()}
+            </>
+        )
+    }
 }
 
 export default CryptoContainer;
